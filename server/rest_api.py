992bb321b84c50e2af957e4b86a40c433389ab94
Aown Raza <aown.raza963@gmail.com>
2024-06-20 18:24:54 +0200

    Added open to network.

--- server/rest_api.py
+++ server/rest_api.py
@@ -95,4 +95,4 @@ def stop_song():
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
